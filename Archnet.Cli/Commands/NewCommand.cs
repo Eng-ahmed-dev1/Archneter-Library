14ad53ddaaefbdc0d90666f21cbbc5ad0dc46b95
Ahmed <ahmed.devmail1@gmail.com>
2026-06-21 22:43:52 +0300

    Refactor NewCommand to improve argument handling and streamline project generation

--- Archnet.Cli/Commands/NewCommand.cs
+++ Archnet.Cli/Commands/NewCommand.cs
@@ -1,20 +1,41 @@
 using Archnet.Cli.Services;
+using Archnet.Core.Enums;
+using Archnet.Core.Models;
 namespace Archnet.Cli.Commands
 {
     public class NewCommand : IArchCommand
     {
         public string Name => "new";
-
-        public async Task ExecuteAsync()
+        private string? GetArg(string[] args, string name)
         {
-            var wizard = new ProjectWizardService();
-            var facory = new GeneratorFactory();
-            var options = wizard.Create();
-            var generator = facory.Get(options.Architecture);
-            await generator.GenerateAsync(options);
-            Console.WriteLine();
-            Console.WriteLine("Project Created Successfully..");
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == name && i + 1 < args.Length)
+                    return args[i + 1];
+            }
 
+            return null;
+        }
+        public async Task ExecuteAsync(string[] args)
+        {
+            var projectName = args.Length > 0 ? args[0] : "DefaultProject";
+
+            var arch = GetArg(args, "--arch") ?? "clean";
+            var tests = GetArg(args, "--tests") == "true";
+
+            var options = new ProjectOptions
+            {
+                ProjectName = projectName,
+                Architecture = ArchitectureType.CleanArchitecture,
+                GenerateTests = tests
+            };
+
+            var factory = new GeneratorFactory();
+            var generator = factory.Get(options.Architecture);
+
+            await generator.GenerateAsync(options);
+
+            Console.WriteLine("Project Created Successfully..");
         }
     }
 }
